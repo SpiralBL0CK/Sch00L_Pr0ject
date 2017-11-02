c2dbededcddf76e4fa3a46d9c15d2cf4dcea0bb5
Vatafu Vladut <vladvatafu9@gmail.com>
2017-11-02 13:17:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+#later wil be update 
+import urllib
+
+def 
+
+def inject_custom_cookie():
+    try:
+        url = urlib.request(raw_input())
+        url.add_header('Cookie',raw_input())
+        info = urllib.urlopen(url)
+    except URLError:
+        print("cant connect to the wesite")
