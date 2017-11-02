64edf646a3927c5a30706546fec7826d6641b17f
Vatafu Vladut <vladvatafu9@gmail.com>
2017-11-02 13:34:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-
-#later wil be update 
 import urllib
 
-def 
+import platform
+import sys
+
+def inject_custom_packets(x,z):
+    try:
+        import scapy
+        from scapy import *
+    except ImportError:
+        print("cant connect,maybe you are missing a packet.Check your dependencies")        
+    #test if packet can be send
+    sr1(IP(dst=x)/z)
+    
+    
+def get_current_version():
+    current_version = platform.python_version()
+    if '2.7' in current_version:
+        print("sorry buddy you'll need to get python 3.6 to be able to run it')
+              
 
 def inject_custom_cookie():
     try:
@@ -11,3 +26,6 @@ def inject_custom_cookie():
         info = urllib.urlopen(url)
     except URLError:
         print("cant connect to the wesite")
+
+def main():
+    
